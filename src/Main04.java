ace6b883984a49c2076e1a660860f19fb5ff410b
Adeonthebeat <fowsdw@naver.com>
2022-08-23 23:33:23 +0900

    아데 인프런 20220823

--- src/Main04.java
+++ src/Main04.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main04 {
@@ -26,4 +27,50 @@ public class Main04 {
             System.out.println(main.solution(str));
         }
     }
+
+    /**
+     * 단어 뒤집기
+     * @param str
+     * @return
+     */
+//    public ArrayList<String> solution(int n, String[] str){
+//        ArrayList<String> ret = new ArrayList<String>();
+//        for(String x : str){
+//            String temp = new StringBuilder(x).reverse().toString();
+//            ret.add(temp);
+//        }
+//
+////        for(String x : str){
+////            char[] s = x.toCharArray();
+////            int lt = 0, rt = x.length() - 1;
+////            while(lt < rt){
+////                char temp = s[lt];
+////                s[lt] = s[rt];
+////                s[rt] = temp;
+////                lt++;
+////                rt--;
+////            }
+////            String temp = String.valueOf(s);
+////            ret.add(temp);
+////        }
+//        return ret;
+//    };
+//
+//    public static void main(String[] args) {
+//        Main04 main = new Main04();
+//        Scanner input = new Scanner(System.in);
+//
+//        int tot = input.nextInt();
+//        String[] str = new String[tot];
+//
+//        for(int i = 0; i < tot; i++){
+//            str[i] = input.next();
+//        }
+//
+//        // foreach
+//        for(String x : main.solution(tot, str)){
+//            System.out.println(x);
+//        }
+//
+//    }
 }
---
+++ src/Main05.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main05 {
+
+    /**
+     * 특수 문자 뒤집기
+     * @param str
+     * @return
+     */
+    public String solution(String str){
+        String ret = "";
+        char[] charArray = str.toCharArray();
+        int lt = 0, rt = str.length() - 1;
+
+        while(lt < rt){
+            // 특수문자 체크
+            if(!Character.isAlphabetic(charArray[lt])){
+                lt++;
+            } else if(!Character.isAlphabetic(charArray[rt])){
+                rt--;
+            } else {
+                char temp = charArray[lt];
+                charArray[lt] = charArray[rt];
+                charArray[rt] = temp;
+                lt++;
+                rt--;
+            }
+        }
+        String temp = String.valueOf(charArray);
+        ret += temp;
+
+        return ret;
+    };
+
+    // a#b!GE*T@S
+    public static void main(String[] args) {
+        Main05 main = new Main05();
+        Scanner input = new Scanner(System.in);
+        String str = input.next();
+        System.out.println(main.solution(str));
+
+    }
+}
