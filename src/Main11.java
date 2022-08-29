b107c45ecce9082d393b6616e32d66feeaa9b0f0
Adeonthebeat <fowsdw@naver.com>
2022-08-29 22:54:09 +0900

    아데 인프런 20220829 - 아데 복습날 20220830 ~ 20220831

---
+++ src/Main11.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main11 {
+
+    /**
+     * 문자열 압축
+     * @param str
+     * @return
+     */
+    public String solution(String str){
+        String result = "";
+
+        str = str + " ";
+        int cnt = 1;
+
+        for(int i = 0; i < str.length() - 1; i++){
+
+            if(str.charAt(i) == str.charAt(i+1)){
+                cnt++;
+            } else {
+
+                result += str.charAt(i);
+
+                if(cnt > 1){
+                    result += String.valueOf(cnt);
+                }
+                cnt = 1;
+            }
+        }
+        return result;
+    };
+
+    // KKHSSSSSSSE ->> K2HS7E
+    // KSTTTSEEKFKKKDJJGG ->> KST3SE2KFK3DJ2G2
+    public static void main(String[] args) {
+        Main11 main = new Main11();
+        Scanner input = new Scanner(System.in);
+        String str = input.next();
+        System.out.println(main.solution(str));
+    }
+}
---
+++ src/Main12.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main12 {
+
+    /**
+     * 문자열 압축
+     * @param str
+     * @return
+     */
+    public String solution(int cnt, String str){
+        String result = "";
+
+        // split
+        for(int i = 0; i < cnt; i++){
+            // split && replace(# = 1 * = 0)
+            String temp = str.substring(0, 7).replace('#', '1').replace('*', '0');
+
+            // 이진수 -> 십진수
+            int num = Integer.parseInt(temp, 2);
+
+            // int를 알바펫화 후, 한단어씩 더해주기
+            result += (char) num;
+            str = str.substring(7);
+        }
+        return result;
+    };
+
+    // 4
+    // #****###**#####**#####**##**
+    // -->> COOL
+    public static void main(String[] args) {
+        Main12 main = new Main12();
+        Scanner input = new Scanner(System.in);
+        int cnt = input.nextInt();
+        String str = input.next();
+        System.out.println(main.solution(cnt, str));
+    }
+}
