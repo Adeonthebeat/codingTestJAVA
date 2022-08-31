b7391db925103b42227ad14ca4c26911cc07ce34
Adeonthebeat <fowsdw@naver.com>
2022-08-31 23:23:24 +0900

    아데 복습날

--- src/practice01_String/Main004.java
+++ src/practice01_String/Main004.java
@@ -2,15 +2,67 @@ package practice01_String;
 
 import com.sun.tools.javac.Main;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * [단어 뒤집기]
+ */
 public class Main004 {
 
+//    public String solution(String str){
+//        String result = "";
+//
+//        for(int i = str.length() -1; i >= 0; i--){
+//            result += str.charAt(i);
+//        }
+//
+//        return result;
+//    }
+
+    public ArrayList<String> solution(int tot, String[] str){
+        ArrayList<String> result = new ArrayList<String>();
+
+//        for(String x : str){
+//            String temp = new StringBuilder(x).reverse().toString();
+//            result.add(temp);
+//        }
+
+        for(String x : str){
+            char[] s = x.toCharArray();
+            int lt = 0, rt = x.length() - 1;
+
+            while(lt < rt){
+                char temp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = temp;
+                lt++;
+                rt--;
+            }
+            String temp = String.valueOf(s);
+            result.add(temp);
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         Main004 main = new Main004();
         Scanner input = new Scanner(System.in);
+        int cnt = input.nextInt();
 
+//        for(int i = 0; i < cnt; i++){
+//            String str = input.next();
+//            System.out.println(main.solution(str));
+//        }
+        String[] strList = new String[cnt];
+        for(int i = 0; i < cnt; i++){
+            strList[i] = input.next();
+        }
+
+        for(String x : main.solution(cnt, strList)){
+            System.out.println(x);
+        }
     }
 
 }
