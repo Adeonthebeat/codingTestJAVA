df29bb0c2a03a295d596241b2e050b4121fb580e
Adeonthebeat <fowsdw@naver.com>
2023-01-16 23:12:30 +0900

    ade commit

--- src/practice07/Main012.java
+++ src/practice07/Main012.java
@@ -1,14 +1,16 @@
-package lecture07;
+package practice07;
 
-import java.util.*;
 
-/**
- * $$$$
- * [경로 탐색(인접리스트)] - 체크리스트
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/** $$$
+ * [경로 탐색(인접리스트)] - DFS | 체크리스트
  */
-public class Main12 {
+public class Main012 {
 
     static int n, m, result = 0;
+
     static ArrayList<ArrayList<Integer>> graph;
     static int[] ch;
 
@@ -17,8 +19,9 @@ public class Main12 {
         if(v == n) {
             result++;
         } else {
-            // 가장 중요 - v정점에서 i정점으로 이동할 때 | 노드가 있으며, 방문한 적이 없을 때!
+            // 가장 중요 - v정점에서 i정점으로 이동할 때
             for(int nv : graph.get(v)) {
+                // 노드가 있으며, 방문한 적이 없을 때!
                 if(ch[nv] == 0) {
                     ch[nv] = 1;
                     DFS(nv);
@@ -29,20 +32,20 @@ public class Main12 {
 
     }
 
-/*
-5 9
-1 2
-1 3
-1 4
-2 1
-2 3
-2 5
-3 4
-4 2
-4 5
-*/
+    /**
+     5 9
+     1 2
+     1 3
+     1 4
+     2 1
+     2 3
+     2 5
+     3 4
+     4 2
+     4 5
+     */
     public static void main(String[] args) {
-        Main12 main = new Main12();
+        Main012 main = new Main012();
         Scanner input = new Scanner(System.in);
 
         n = input.nextInt();
@@ -50,21 +53,20 @@ public class Main12 {
 
         ch = new int[n+1];
 
-        // 리스트를 저장하는 리스트
         graph = new ArrayList<ArrayList<Integer>>();
 
-        // 노드 개수만큼 리스트 객체 생성 | 가장 중요
+        // 리스트를 저장하는 리스트
         for(int i = 0; i <= n; i++) {
             graph.add(new ArrayList<Integer>());
         }
 
+        // 노드 개수만큼 리스트 객체 생성 | 가장 중요
         for(int i = 0; i < m; i++) {
             int a = input.nextInt();
             int b = input.nextInt();
 
             // * 1개의 정점에 하나씩 추가 | 모두 5개 리스트 | 9개 값 저장
             graph.get(a).add(b);
-
         }
 
         ch[1] = 1;
