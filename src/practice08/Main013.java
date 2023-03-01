25a3821e8675bc2580efa791f1dda12881b2b80b
Adeonthebeat <fowsdw@naver.com>
2023-03-01 22:37:07 +0900

    ade commit

--- src/practice08/Main013.java
+++ src/practice08/Main013.java
@@ -1,24 +1,26 @@
-package lecture08;
+package practice08;
 
 import java.util.Scanner;
 
-/**
- * [섬나라 아일랜드] - DFS
+/** $$
+ * [섬나라 아일랜드] - 개수 구하기
  */
-public class Main13 {
+public class Main013 {
 
-    static int n, result;
+    static int n, result = 0;
     static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1}; // 시계방향
-    static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1}; // 시계방향
+    static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1}; // 반시계방향
 
     public void DFS(int x, int y, int[][] board) {
         for(int i = 0; i < 8; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+            for(int j = 0; j < 8; j++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-            if(nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1){
-                board[nx][ny] = 0;
-                DFS(nx, ny, board);
+                if(nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1) {
+                    board[nx][ny] = 0;
+                    DFS(nx, ny, board);
+                }
             }
         }
     }
@@ -26,28 +28,27 @@ public class Main13 {
     public void solution(int[][] board) {
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                if(board[i][j] == 1){
-                    result++;
+                if(board[i][j] == 1) {
+                    result += 1;
                     board[i][j] = 0; // 출발하는 지점 = 0
                     DFS(i, j, board);
                 }
             }
         }
-
     }
 
-    /**
-     7
-     1 1 0 0 0 1 0
-     0 1 1 0 1 1 0
-     0 1 0 0 0 0 0
-     0 0 0 1 0 1 1
-     1 1 0 1 1 0 0
-     1 0 0 0 1 0 0
-     1 0 1 0 1 0 0
-     */
+    /*  각 섬은 1로 표시 | 0은 바다 | 상하좌 우와 대각선으로 연결
+    7
+    1 1 0 0 0 1 0
+    0 1 1 0 1 1 0
+    0 1 0 0 0 0 0
+    0 0 0 1 0 1 1
+    1 1 0 1 1 0 0
+    1 0 0 0 1 0 0
+    1 0 1 0 1 0 0
+    */
     public static void main(String[] args) {
-        Main13 main = new Main13();
+        Main013 main = new Main013();
         Scanner input = new Scanner(System.in);
 
         n = input.nextInt();
@@ -62,6 +63,7 @@ public class Main13 {
         main.solution(board);
 
         System.out.println(result);
+
     }
 
 }
