c6c1dcddd38dead0a3736679b8d74a1ac8dc177b
Adeonthebeat <fowsdw@naver.com>
2023-05-17 22:03:21 +0900

    ade commit

---
+++ src/test03/Main03.java
@@ -0,0 +1,46 @@
+package test03;
+
+import java.util.Scanner;
+
+/**
+ * [최대 매출]
+ */
+public class Main03 {
+
+    public int solution(int n, int k, int[] arr) {
+        int result = 0, sum = 0;
+
+        for(int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        for(int i = k; i < n; i++) {
+            sum += arr[i] - arr[i-k];
+            result = Math.max(result, sum);
+        }
+
+        return result;
+    }
+
+/*
+10 3
+12 15 11 20 25 10 20 19 13 15
+*/
+    public static void main(String[] args) {
+        Main03 main = new Main03();
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int k = input.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        System.out.println(main.solution(n, k, arr));
+
+    }
+
+}
---
+++ src/test03/Main04.java
@@ -0,0 +1,52 @@
+package test03;
+
+import java.util.Scanner;
+
+/**
+ * [연속 부분수열]
+ */
+public class Main04 {
+
+    public int solution(int n, int m, int[] arr) {
+        int result = 0, sum = 0, lt = 0;
+
+        for(int rt = 0; rt < n; rt++) {
+            sum += arr[rt];
+
+            if(sum == m) {
+                result++;
+            }
+
+            while(sum > m) {
+                sum -= arr[lt++];
+
+                if(sum == m) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+/*
+8 6
+1 2 1 3 1 1 1 2
+*/
+    public static void main(String[] args) {
+        Main04 main = new Main04();
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int m = input.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        System.out.println(main.solution(n, m, arr));
+
+    }
+
+}
